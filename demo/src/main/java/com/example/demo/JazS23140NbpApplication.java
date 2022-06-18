fdcc583fba723c6b4b21a034834014690079e091
YamatooX <jarbatorek@gmail.com>
2022-06-18 09:35:00 +0200

    init

---
+++ demo/src/main/java/com/example/demo/JazS23140NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS23140NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS23140NbpApplication.class, args);
+	}
+
+}
